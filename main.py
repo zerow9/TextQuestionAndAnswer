415dadceccc685e7f938069048f8598451fcbcfd
zerow9 <https://github.com/zerow9/TextQuestionAndAnswer>
2018-02-08 11:46:23 +0800

    second commit

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main(noStructureDataPath,semiStructuredDataPathFolder):
     :param noStructureDataPath: 只能是无结构化的数据文件
     :param semiStructuredDataPathFolder:可以是半结构化的数据文件，也可以是半结构化的数据文件夹
     :return:问题的json数据返回；key:question，value:answer
+    :article_sturction_judge_main：段间问答
+    :sentencesMain：句内问答
+    :semi_structured_main：半结构化数据问答
     '''
     dictMerged = dict(dict(article_sturction_judge_main(noStructureDataPath),
                            **sentencesMain(noStructureDataPath)),
@@ -25,4 +28,3 @@ if __name__ == '__main__':
     # print(len(article_sturction_judge_main("nineteenReportDocuments.docx").keys()))
     # print(len(sentencesMain("nineteenReportDocuments.docx").keys()))
     # print(len(semi_structured_main("text").keys()))
-
